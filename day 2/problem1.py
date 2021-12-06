683d9b576d9147ded5101e5d8e88a4f2035f19f6
Noah Van Der Weide <nvanderweide@wisc.edu>
2021-12-06 13:39:15 -0600

    day 2 done

---
+++ day 2/problem1.py
@@ -0,0 +1,26 @@
+horizontal_sum = 0
+depth_sum = 0
+lines = []
+
+# Read the input
+print('enter the values')
+while True:
+    line = input()
+    if line:
+        lines.append(line)
+    else:
+        break
+
+# Calculate the sum of the horizontal and depth
+for line in lines:
+    line = line.split()
+    if line[0] == 'forward':
+        horizontal_sum += int(line[1])
+    elif line[0] == 'up':
+        depth_sum -= int(line[1])
+    elif line[0] == 'down':
+        depth_sum += int(line[1])
+
+print("horizontal_sum:", horizontal_sum)
+print("depth_sum:", depth_sum)
+print("multiplied_sum:", horizontal_sum * depth_sum)
